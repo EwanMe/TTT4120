34fb8299f8e3ebc6e4769230480ab0b518e1380a
Hallvard Jensen <hallvard.jensen@gmail.com>
2025-10-17 17:30:45 +0200

    finish problem-1

--- assignment-6/problem-1.py
+++ assignment-6/problem-1.py
@@ -1,21 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.fft
-import scipy.signal
-
-N_x = 28
 
 
-def X_f():
-    N_x = 1000
-    f = np.arange(0, N_x, 0.001)
-    return (1 - (0.9 * np.e) ** (-2j * np.pi * f)) ** N_x / (
-        1 - 0.9 * np.e ** (2j * np.pi * f)
+def X_f(N):
+    f = np.linspace(0, 1, 10000)
+    return f, (1 - (0.9 * np.exp(-2j * np.pi * f)) ** N) / (
+        1 - 0.9 * np.exp(-2j * np.pi * f)
     )
 
 
-def plot_signal_freq_rsp(X, title: str, yax: str, xax: str):
+def plot_dft(X, title: str, yax: str, xax: str):
     plt.subplot()
+    plt.title(title)
     plt.stem(X)
     plt.title(title)
     plt.ylabel(yax)
@@ -23,25 +20,27 @@ def plot_signal_freq_rsp(X, title: str, yax: str, xax: str):
     plt.show()
 
 
-def x():
+def plot_dtft(N):
+    f, X = X_f(N)
+    plt.plot(f, np.abs(X))
+    plt.title("DTFT")
+    plt.ylabel("|X(f)|")
+    plt.xlabel("f")
+    plt.show()
+
+
+def x(N_x):
     n = np.arange(0, N_x - 1)
     return 0.9**n
 
 
 def subtask_b():
-    plt.subplot()
-    plt.plot(np.abs(X_f()), scalex=0.001)
-    plt.title("title")
-    plt.ylabel("|X(f)|")
-    plt.xlabel("f")
-    plt.show()
-
+    N_x = 28
+    plot_dtft(N_x)
     for div in [4, 2, 1, 1 / 2]:
         fft_len = int(N_x * (1 / div))
-        X_k = scipy.fft.fft(x(), n=fft_len)
-        plot_signal_freq_rsp(
-            X_k, f"FFT length: N_x/{div}={fft_len}", "X(k)", "k"
-        )
+        X_k = scipy.fft.fft(x(N_x), n=fft_len)
+        plot_dft(X_k, f"DFT: FFT length: N_x/{div}={fft_len}", "X(k)", "k")
 
 
 def main():
