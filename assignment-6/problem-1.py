b1e85d502c694c3036663379824dc482f4833c9f
Hallvard Jensen <hallvard.jensen@gmail.com>
2025-10-17 16:59:44 +0200

    wip: subtask d

--- assignment-6/problem-1.py
+++ assignment-6/problem-1.py
@@ -1,13 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.fft
+import scipy.signal
 
 N_x = 28
 
 
+def X_f():
+    N_x = 1000
+    f = np.arange(0, N_x, 0.001)
+    return (1 - (0.9 * np.e) ** (-2j * np.pi * f)) ** N_x / (
+        1 - 0.9 * np.e ** (2j * np.pi * f)
+    )
+
+
 def plot_signal_freq_rsp(X, title: str, yax: str, xax: str):
     plt.subplot()
-    plt.plot(X)
+    plt.stem(X)
     plt.title(title)
     plt.ylabel(yax)
     plt.xlabel(xax)
@@ -20,10 +29,19 @@ def x():
 
 
 def subtask_b():
+    plt.subplot()
+    plt.plot(np.abs(X_f()), scalex=0.001)
+    plt.title("title")
+    plt.ylabel("|X(f)|")
+    plt.xlabel("f")
+    plt.show()
+
     for div in [4, 2, 1, 1 / 2]:
         fft_len = int(N_x * (1 / div))
-        X = scipy.fft.fft(x(), n=fft_len)
-        plot_signal_freq_rsp(X, f"FFT length: N_x/{div}={fft_len}", "X(f)", "f")
+        X_k = scipy.fft.fft(x(), n=fft_len)
+        plot_signal_freq_rsp(
+            X_k, f"FFT length: N_x/{div}={fft_len}", "X(k)", "k"
+        )
 
 
 def main():
