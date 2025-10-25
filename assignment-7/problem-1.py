26031ee2596f38c44c2fd0479de57548a2ee411a
Hallvard Jensen <hallvard.jensen@gmail.com>
2025-10-25 17:01:38 +0200

    wip

--- assignment-7/problem-1.py
+++ assignment-7/problem-1.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
+import scipy.fft
+import scipy.signal
 import statsmodels.api as sm
 
 
@@ -8,9 +10,10 @@ def white_binary_noise(size: int) -> npt.NDArray[np.float64]:
     return np.random.choice([-1, 1], size=size)
 
 
-def white_gaussian_noise(size: int) -> npt.NDArray[np.float64]:
+def white_gaussian_noise(
+    size: int, std: float = 1.0
+) -> npt.NDArray[np.float64]:
     mean = 0
-    std = 1
     return np.random.normal(mean, std, size=size)
 
 
@@ -18,12 +21,18 @@ def white_uniform_noise(size: int) -> npt.NDArray[np.float64]:
     return np.random.uniform(-1, 1, size=size)
 
 
-def plot(f, x=None, title: str = "", xlabel: str = "", ylabel: str = ""):
+def plot(
+    *f,
+    x: npt.NDArray = None,
+    title: str = "",
+    xlabel: str = "",
+    ylabel: str = "",
+):
     ax = plt.subplot()
     if x is None:
-        ax.plot(f)
+        ax.plot(np.arange(max([len(ff) for ff in f])), *f)
     else:
-        ax.plot(x, f)
+        ax.plot(x, *f)
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
@@ -46,13 +55,13 @@ def ac_range(x: npt.NDArray[np.float64], r: npt.NDArray):
     return ac[center + r[0] : center + r[-1] + 1]
 
 
-def subtask_a() -> None:
+def problem_1_subtask_a() -> None:
     plot(white_binary_noise(100), title="Problem 1a): Binary noise")
     plot(white_gaussian_noise(100), title="Problem 1a): Gaussian noise")
     plot(white_uniform_noise(100), title="Problem 1a): Uniform noise")
 
 
-def subtask_c() -> None:
+def problem_1_subtask_c() -> None:
     length = 20000
     r = np.arange(-10, 11)  # [-10,10]
 
@@ -84,9 +93,51 @@ def subtask_c() -> None:
     )
 
 
+def y_xx(r: npt.NDArray):
+    return 3 / 4 * (-1 / 2) ** np.abs(r)
+
+
+# def Gamma(f: float):
+#     return 9 / 16 * 1 / ((1 + 1 / 2 * f**-1) * (1 + 1 / 2 * f))
+
+
+def Gamma(w: npt.NDArray):
+    return 9 / 16 * 1 / ((5 / 4) + np.cos(w))
+
+
+def problem_2_subtask_c():
+    N = 20000
+    w = white_gaussian_noise(N, 3 / 4)
+    x = scipy.signal.lfilter([1], [1, 1 / 2], w)
+
+    r = np.arange(-10, 11)
+    e_mean = np.mean(x)
+    e_ac = ac_range(x, r)
+    e_pds = scipy.fft.fft(e_ac, n=200, norm="forward")
+
+    print(f"Mean: {e_mean}")
+    # plot(y_xx(r), e_ac, x=r, title="Autocorrelation")
+
+    # plot(e_pds, Gamma(r), r, title="PDS")
+
+    ax = plt.subplot()
+    print(np.abs(e_pds))
+    l = np.arange(-5, 5)
+    ax.stem(np.abs(e_pds))
+    plt.show()
+
+    # freqs, times, spectrogram = scipy.signal.spectrogram(x)
+    # ax = plt.subplot()
+    # ax.imshow(spectrogram, aspect="auto", cmap="hot_r", origin="lower")
+    # plt.title("Spectrogram")
+    # plt.tight_layout()
+    # plt.show()
+
+
 def main() -> None:
-    subtask_a()
-    subtask_c()
+    # problem_1_subtask_a()
+    # problem_1_subtask_c()
+    problem_2_subtask_c()
 
 
 if __name__ == "__main__":
