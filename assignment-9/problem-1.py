4b24ea2d140bdaecfcd927721c9f2ed04fcdb5fd
Hallvard Jensen <hallvard.jensen@gmail.com>
2025-11-10 22:28:37 +0100

    init assignment 9

---
+++ assignment-9/problem-1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def h(n):
+    pass
+
+
+def main():
+    N = 10
+    n = np.linspace(0, 1)
+    hamming = np.hamming(N)
+    h(n)
+
+
+if __name__ == "__main__":
+    main()
